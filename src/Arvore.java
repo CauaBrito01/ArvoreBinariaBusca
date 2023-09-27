4373aa27030b95d6c9d30fdd7ebb859d0395e62e
Cauã de França Brito <cauafranca115@gmail.com>
2023-09-27 19:30:24 -0300

    comentar codigo

--- src/Arvore.java
+++ src/Arvore.java
@@ -4,26 +4,26 @@ public class Arvore<TIPO extends Comparable<TIPO>> {
     public void adicionar(TIPO valor) {
         Elemento<TIPO> novoElemento = new Elemento<>(valor);
         if (this.raiz == null) {
-            this.raiz = novoElemento;
+            this.raiz = novoElemento; // Se a árvore estiver vazia, o novo elemento se torna a raiz
         } else {
             Elemento<TIPO> atual = this.raiz;
 
             while (true) {
                 int comparacao = valor.compareTo(atual.getValor());
                 if (comparacao == 0) {
-                    break;
+                    break; // Valor já existe na árvore, saia do loop
                 }
 
                 if (comparacao < 0) {
                     if (atual.getEsquerda() == null) {
-                        atual.setEsquerda(novoElemento);
+                        atual.setEsquerda(novoElemento); // Inserir à esquerda se o valor for menor
                         break;
                     }
 
                     atual = atual.getEsquerda();
                 } else {
                     if (atual.getDireita() == null) {
-                        atual.setDireita(novoElemento);
+                        atual.setDireita(novoElemento); // Inserir à direita se o valor for maior
                         break;
                     }
 
@@ -34,19 +34,20 @@ public class Arvore<TIPO extends Comparable<TIPO>> {
     }
 
     public boolean buscar(TIPO valor) {
-        return this.buscarRecursivo(this.raiz, valor);
+        return this.buscarRecursivo(this.raiz, valor); // Inicia a busca recursiva a partir da raiz
     }
 
     private boolean buscarRecursivo(Elemento<TIPO> elemento, TIPO valor) {
         if (elemento == null) {
-            return false;
+            return false; // Valor não encontrado na árvore
         } else {
             int comparacao = valor.compareTo(elemento.getValor());
             if (comparacao == 0) {
-                return true;
+                return true; // Valor encontrado na árvore
             } else {
                 return comparacao < 0 ? this.buscarRecursivo(elemento.getEsquerda(), valor)
                         : this.buscarRecursivo(elemento.getDireita(), valor);
+                // Continua a busca na subárvore esquerda se for menor, senão, na subárvore direita
             }
         }
     }
@@ -60,38 +61,42 @@ public class Arvore<TIPO extends Comparable<TIPO>> {
             this.imprimirEmOrdemRecursivo(elemento.getDireita(), nivel + 1);
 
             for (int i = 0; i < nivel; ++i) {
-                System.out.print("   ");
+                System.out.print("   "); // Espaços para formatação
             }
 
-            System.out.println(elemento.getValor());
+            System.out.println(elemento.getValor()); // Imprime o valor do elemento
             this.imprimirEmOrdemRecursivo(elemento.getEsquerda(), nivel + 1);
+            // Continua a impressão na subárvore esquerda
         }
     }
 
     public void remover(TIPO valor) {
-        this.raiz = this.removerRecursivo(this.raiz, valor);
+        this.raiz = this.removerRecursivo(this.raiz, valor); // Inicia a remoção recursiva a partir da raiz
     }
 
     private Elemento<TIPO> removerRecursivo(Elemento<TIPO> elemento, TIPO valor) {
         if (elemento == null) {
-            return elemento;
+            return elemento; // Valor não encontrado, retorne o elemento atual
         } else {
             int comparacao = valor.compareTo(elemento.getValor());
             if (comparacao < 0) {
                 elemento.setEsquerda(this.removerRecursivo(elemento.getEsquerda(), valor));
+                // Continue a remoção na subárvore esquerda se for menor
             } else if (comparacao > 0) {
                 elemento.setDireita(this.removerRecursivo(elemento.getDireita(), valor));
+                // Continue a remoção na subárvore direita se for maior
             } else {
                 if (elemento.getEsquerda() == null && elemento.getDireita() == null) {
-                    return null;
+                    return null; // Nó folha, simplesmente remova-o
                 } else if (elemento.getEsquerda() == null) {
-                    return elemento.getDireita();
+                    return elemento.getDireita(); // Nó com apenas um filho à direita
                 } else if (elemento.getDireita() == null) {
-                    return elemento.getEsquerda();
+                    return elemento.getEsquerda(); // Nó com apenas um filho à esquerda
                 } else {
                     Elemento<TIPO> minimoDireita = this.encontrarMinimo(elemento.getDireita());
                     elemento.setValor(minimoDireita.getValor());
                     elemento.setDireita(this.removerRecursivo(elemento.getDireita(), minimoDireita.getValor()));
+                    // Substitui o valor pelo mínimo da subárvore direita e continua a remoção nessa subárvore
                 }
             }
 
@@ -104,6 +109,6 @@ public class Arvore<TIPO extends Comparable<TIPO>> {
             elemento = elemento.getEsquerda();
         }
 
-        return elemento;
+        return elemento; // Encontra e retorna o elemento mínimo da subárvore esquerda
     }
 }
--- src/Elemento.java
+++ src/Elemento.java
@@ -1,35 +1,35 @@
 public class Elemento<TIPO> {
-    private TIPO valor;
-    private Elemento<TIPO> esquerda;
-    private Elemento<TIPO> direita;
+    private TIPO valor;         // Armazena o valor do elemento
+    private Elemento<TIPO> esquerda; // Referência ao elemento à esquerda
+    private Elemento<TIPO> direita;  // Referência ao elemento à direita
 
-    public Elemento(TIPO novoValor) {
-        this.valor = novoValor;
-        this.esquerda = null;
-        this.direita = null;
+    public Elemento(TIPO novoValor) { // Construtor da classe Elemento
+        this.valor = novoValor;      // Inicializa o valor do elemento com o valor especificado
+        this.esquerda = null;        // Inicializa a referência à esquerda como nula
+        this.direita = null;         // Inicializa a referência à direita como nula
     }
 
-    public TIPO getValor() {
+    public TIPO getValor() {         // Método para obter o valor do elemento
         return this.valor;
     }
 
-    public void setValor(TIPO novoValor) {
+    public void setValor(TIPO novoValor) { // Método para definir o valor do elemento
         this.valor = novoValor;
     }
 
-    public Elemento<TIPO> getEsquerda() {
+    public Elemento<TIPO> getEsquerda() { // Método para obter a referência ao elemento à esquerda
         return this.esquerda;
     }
 
-    public void setEsquerda(Elemento<TIPO> esquerda) {
+    public void setEsquerda(Elemento<TIPO> esquerda) { // Método para definir a referência ao elemento à esquerda
         this.esquerda = esquerda;
     }
 
-    public Elemento<TIPO> getDireita() {
+    public Elemento<TIPO> getDireita() { // Método para obter a referência ao elemento à direita
         return this.direita;
     }
 
-    public void setDireita(Elemento<TIPO> direita) {
+    public void setDireita(Elemento<TIPO> direita) { // Método para definir a referência ao elemento à direita
         this.direita = direita;
     }
 }
